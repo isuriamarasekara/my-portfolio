15d9ceb0675eadc65e1f6e2b275f6baf316c11d2
kasunmadhuranga <ggkasun92@gmail.com>
2025-07-16 12:30:47 +0530

    main change

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // IMPORTANT
-import App from './App';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom'; // IMPORTANT
+// import App from './App';
+// import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter> {/* 👈 Must wrap App */}
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <BrowserRouter> {/* 👈 Must wrap App */}
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Router>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    </Router>
+  </StrictMode>
+)
